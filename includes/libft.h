d690a08c8323783eb8b2bc346ad4ac945d4212e4
anduuin <jcm2232@gmail.com>
2022-01-16 19:45:50 +0900

    move ft_malloc / ft_perror to libft

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 19:19:58 by cjeon             #+#    #+#             */
-/*   Updated: 2021/12/24 19:09:44 by cjeon            ###   ########.fr       */
+/*   Updated: 2022/01/16 17:41:03 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define LIBFT_H
 
 # include <stddef.h>
+
+# define PERROR_BUF_SIZE	128
+# define MALLOC_FAIL_PREFIX "minishell"
 
 typedef struct s_list
 {
@@ -65,4 +68,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+void	*ft_malloc(size_t size);
+void	ft_perror(const char *prefix);
+void	ft_perror_texit(const char *prefix, int status);
+void	ft_perror_custom(const char *prefix, const char *str);
+void	ft_perror_custom_texit(const char *prefix,
+			const char *str, int status);
 #endif
