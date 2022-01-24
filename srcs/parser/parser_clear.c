80a1751979ac1567b7e38ac54f09f3d94f1e72c4
anduuin <jcm2232@gmail.com>
2022-01-24 23:25:55 +0900

    Add clear function for command_node

---
+++ srcs/parser/parser_clear.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_clear.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 10:48:59 by cjeon             #+#    #+#             */
+/*   Updated: 2022/01/23 11:06:30 by cjeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "parser.h"
+
+void pipeline_clear(t_pipeline *pipeline)
+{
+	free(pipeline->childs);
+	command_clear(pipeline->commands, pipeline->len);
+	free(pipeline);
+}
+
+void command_node_clear(t_command_node *node)
+{
+	if (node->type == C_PIPELINE)
+		pipeline_clear(node->pipeline);
+	free(node);
+}
+
+void line_info_clear(t_line_info *li)
+{
+	t_command_node *curr;
+	t_command_node *next;
+
+	curr = li->head;
+	while (curr)
+	{
+		next = curr->next;
+		command_node_clear(curr);
+		curr = next;
+	}
+}
---
+++ srcs/parser/parser_cmd_clear.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_cmd_clear.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 11:06:18 by cjeon             #+#    #+#             */
+/*   Updated: 2022/01/23 11:06:44 by cjeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "parser.h"
+
+void redir_clear(t_redir *redir)
+{
+	t_redir *curr;
+	t_redir *next;
+
+	curr = redir;
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->target);
+		free(curr);
+		curr = next;
+	}
+}
+
+void data_clear(t_cmd_data data, int type)
+{
+	char **cmd_arr;
+
+	if (type == C_SUBSHELL)
+		free(data.s);
+	else
+	{
+		cmd_arr = data.c;
+		while (*cmd_arr)
+			free(*cmd_arr++);
+		free(data.c);
+	}
+}
+
+void command_clear(t_command *commands, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+	{
+		redir_clear(commands[i].redir);
+		data_clear(commands[i].data, commands[i].type);
+		i++;
+	}
+	free(commands);
+}
