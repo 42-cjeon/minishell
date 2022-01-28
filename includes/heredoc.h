f98d44c59499ce70bd4623a770b157f64d4b45d4
sdreamer <sdreamer0329@gmail.com>
2022-01-28 19:00:36 +0900

    implemented heredoc roughly

---
+++ includes/heredoc.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanelee <hanelee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 18:19:27 by hanelee           #+#    #+#             */
+/*   Updated: 2022/01/28 18:21:24 by hanelee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEREDOC_H
+# define HEREDOC_H
+
+void	heredoc_fmake(const char *fname, const char *lim);
+void	heredoc_fremove(const char *fname);
+
+#endif
