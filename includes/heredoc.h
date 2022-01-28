40cc27e4761927434c2f52a4d19fcaba56a443c5
sdreamer <sdreamer0329@gmail.com>
2022-01-28 19:02:48 +0900

    modified fname to pname

--- includes/heredoc.h
+++ includes/heredoc.h
@@ -6,14 +6,14 @@
 /*   By: hanelee <hanelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 18:19:27 by hanelee           #+#    #+#             */
-/*   Updated: 2022/01/28 18:21:24 by hanelee          ###   ########.fr       */
+/*   Updated: 2022/01/28 19:02:14 by hanelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEREDOC_H
 # define HEREDOC_H
 
-void	heredoc_fmake(const char *fname, const char *lim);
-void	heredoc_fremove(const char *fname);
+void	heredoc_fmake(const char *pname, const char *lim);
+void	heredoc_fremove(const char *pname);
 
 #endif
