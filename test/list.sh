e68a5b19f6a251907b0e5b4215eafeb77986df15
sdreamer <sdreamer0329@gmail.com>
2022-01-30 12:19:26 +0900

    push tester

---
+++ test/list.sh
@@ -0,0 +1,118 @@
+# includes: pipeline, builtin(cd, exit, env, export, unset), redirection
+
+# 시연
+# working history
+
+# !-- builtin - cd & pwd
+cd
+pwd
+cd .
+pwd
+cd ..
+pwd
+cd /bin
+pwd
+cd [PROJ_ROOT]
+pwd	arg						# pwd ignores argument
+cd Makefile					# not a directory
+cd notexist					# not a file or directory
+# ---
+
+# !-- builtin - env
+env
+# ---
+
+# !-- builtin - export
+export
+export TEST01=should_be_replaced
+export TEST01=test01
+export TEST02=test02 TEST03=test03 TEST04=test04
+# ---
+
+# !-- builtin - unset
+unset
+unset NOTEXIST
+unset TEST04
+unset TEST01 TEST02 TEST03
+# ---
+
+# !-- builtin - exit
+exit
+exit 42
+exit 42 53 68
+exit 9223372036854775807
+exit -9223372036854775808
+exit wrong
+exit wrong statement
+# ---
+
+# !-- Return value of a process
+/bin/ls
+echo $?
+/bin/ls filedoesntexist
+echo $?
+expr $? + $?
+# ---
+
+# !-- Environment Path
+unset PATH
+ls							# 외부 프로그램 동작x 시연
+export PATH=/usr/bin
+wc							# /usr/bin 에 존재하는 외부 프로그램을 경로입력 없이 실행 가능한 것을 시연
+export PATH=$PATH:/bin
+ls							# 경로를 지정해주자 다시 ls도 경로없이 실행 가능.
+# ---
+
+# !-- Signals
+ # 비어있는 프롬프트에 ctrl-c		-> new line
+ # 비어있는 프롬프트에 ctrl-\		-> do nothing
+ # 비어있는 프롬프트에 ctrl-d		-> quit shell
+ # 버퍼가 존재하는 프롬프트에 ctrl-c	-> 버퍼 지우고 new line
+ # 버퍼가 존재하는 프롬프트에 ctrl-\	-> do nothing
+ # 버퍼가 존재하는 프롬프트에 ctrl-d	-> do nothing
+ # cat과 같은 blocking cmd 이후 ctrl-c	-> cmd 종료
+ # cat과 같은 blocking cmd 이후 ctrl-\	-> cmd 종료
+ # cat과 같은 blocking cmd 이후 ctrl-d	-> cmd 종료
+# ---
+
+# !-- Double Quotes
+echo "cat lol.c | cat > lol.c"
+echo "cmddoesnotexist -rpak alkjdf"
+# ---
+
+# !-- Single Quotes
+
+# ---
+
+# !-- Redirection
+ # <
+< a cat < b < c			# c 파일만 보임
+
+ # >
+> a ls > b >> c > d
+
+ # <<
+
+ # >>
+
+ # mixed
+echo test > ls >> ls >> ls && echo test >> ls && cat ls
+echo test > ls ; cat ls
+echo test > ls >> ls >> ls ; echo test >> ls; cat ls
+> lol echo test lol; cat lol
+>lol echo > test>lol>test>>lol>test mdr >lol test >test; cat test
+cat < ls
+cat < ls > ls
+# ---
+
+# !-- Pipes
+cat Makefile | grep clean | head -n 5 | cd file_not_exist	# cd: filedoesntexist: No such file or directory (es: 1)
+cat Makefile | grep clean | head -n 5 | cmd_not_exist		# minishell: cmd_not_exist: command not found (es: 127)
+
+cat ../tester/lorem.txt | grep arcu | cat -e
+echo test | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e | cat -e| cat -e| cat -e| cat -e| cat -e| cat -e| cat -e| cat -e| cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e|cat -e
+cat /dev/random | head -c 100 | wc -c
+ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls
+ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls|ls
+# ---
+
---
+++ test/test.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+# Variables
+PROJ_DIR="../"
+PROJ_NAME="minishell"
+PROJ_PNAME="$PROJ_DIR/$PROJ_NAME"
+TIME_INTVL_LONG="2.0"
+TIME_INTVL_SHORT="0.5"
+
+RESET="\033[0m"
+RED="\033[31m"
+GREEN="\033[32m"
+WHITE="\033[37m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+RED_BOLD="\033[1m\033[31m"
+GREEN_BOLD="\033[1m\033[32m"
+WHITE_BOLD="\033[1m\033[37m"
+BLUE_BOLD="\033[1m\033[34m"
+MAGENTA_BOLD="\033[1m\033[35m"
+CYAN_BOLD="\033[1m\033[36m"
+
+# Functions
+function check_norm()
+{
+	printf "$MAGENTA_BOLD [ !- Check norm started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	make -C $PROJ_DIR norm
+
+	printf "$MAGENTA_BOLD [ -- Check norm finished. ]$RESET\n\n\n"
+	sleep $TIME_INTVL_LONG
+}
+
+function compile()
+{
+	printf "$MAGENTA_BOLD [ !- Compile started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	make -C $PROJ_DIR
+
+	printf "$MAGENTA_BOLD [ -- Compile finished. ]$RESET\n\n\n"
+	sleep $TIME_INTVL_LONG
+}
+
+function test_unit()
+{
+	RES_MS=$(echo $@ | $PROJ_PNAME 2>/dev/null)
+	# ES_MS=$?
+	RES_BS=$(echo $@ | bash 2>/dev/null)
+	# ES_BS=$?
+
+	printf "\n[ test $CYAN$@$RESET ] "
+	if [ "$RES_MS" == "$RES_BS" ] && [ "$ES_MS" == "$ES_BS" ]; then
+		printf "$GREEN_BOLD%s$RESET\n" "✓"
+	else
+		printf "$RED_BOLD%s$RESET\n" "✗"
+	fi
+	printf "%s\n" "-------------------------------------------"
+	if [ "$RES_MS" == "$RES_BS" ]; then
+		printf " - ms result :\n%.20s\n" "$RES_MS"
+		printf " - bs result :\n%.20s\n" "$RES_BS"
+		printf "%s\n" "-------------------------------------------"
+	else
+		printf $WHITE" - ms result :\n$RED_BOLD$RES_MS$RESET\n"
+		printf $WHITE" - bs result :\n$GREEN_BOLD$RES_BS$RESET\n"
+		printf "%s\n" "-------------------------------------------"
+		exit
+	fi
+	# if [ "$ES_MS" == "$ES_BS" ]; then
+	# 	printf $WHITE" - ms status : $ES_MS$RESET\n"
+	# 	printf $WHITE" - bs status : $ES_BS$RESET\n"
+	# else
+	# 	printf $WHITE" - ms status : $RED_BOLD$ES_MS$RESET\n"
+	# 	printf $WHITE" - bs status : $GREEN_BOLD$ES_BS$RESET\n"
+	# fi
+	sleep $TIME_INTVL_SHORT
+}
+
+function test_cmd_simple()
+{
+	printf "$MAGENTA_BOLD [ !- Test \"simple command\" started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	test_unit '/bin/ls'
+	test_unit ''
+	test_unit '   \t'
+	
+	printf "\n\n"
+}
+
+function test_cmd_argument()
+{
+	printf "$MAGENTA_BOLD [ !- Test \"command with argument\" started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	test_unit '/bin/ls -al'
+	test_unit 'ls -al'
+	test_unit 'ls -R ../includes'
+
+	printf "\n\n"
+}
+
+function test_builtin_echo()
+{
+	printf "$MAGENTA_BOLD [ !- Test \"echo\" started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	test_unit 'echo '
+	test_unit 'echo test1 test2'
+	test_unit 'echo test1	test2'
+	test_unit 'echo -n test1 test2'
+	test_unit 'echo -n -n -n test1 test2'
+	test_unit 'echo -na test1 test2'
+	test_unit 'echo -nhasdf -aisjdf test1 test2 test3 test4'
+
+	printf "\n\n"
+}
+
+function test_env_expansion()
+{
+	printf "$MAGENTA_BOLD [ !- Test \"env expansion\" started! ]$RESET\n"
+	sleep $TIME_INTVL_LONG
+
+	test_unit 'echo $USER'
+	test_unit 'echo "$USER"'
+	test_unit 'echo "'$USER'"'
+	test_unit 'echo '"$USER"''
+	test_unit "echo '$HOME'"
+	test_unit 'echo "$HOME$LANG$HOME"'
+	test_unit 'echo "$HOME$HOME=lol$HOME"'
+	test_unit 'echo "   $HOME lol $LANG"'
+	test_unit 'echo $HOME$LANG$HOME' 
+	test_unit 'echo $TEST$TEST=lol$TEST""lol'
+	test_unit 'echo "$?TEST"'
+	test_unit 'echo test "" test "" test'
+
+	printf "\n\n"
+}
+
+# Processes
+# check_norm; compile;
+test_cmd_simple
+test_cmd_argument
+test_builtin_echo
+test_env_expansion
+
