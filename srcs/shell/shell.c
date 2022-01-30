b2baca3fce2ceadeb5e294832aa61cc19ada04b0
cjeon <cjeon@student.42seoul.kr>
2022-01-30 12:27:42 +0900

    Add strict rule for test tty

--- srcs/shell/shell.c
+++ srcs/shell/shell.c
@@ -6,7 +6,7 @@
 /*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 14:22:03 by hanelee           #+#    #+#             */
-/*   Updated: 2022/01/30 12:26:11 by cjeon            ###   ########.fr       */
+/*   Updated: 2022/01/30 12:27:08 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	shell_init(t_shell_info *si, const char *envp[])
 	si->default_stdin = ft_dup(STDIN_FILENO);
 	si->default_stdout = ft_dup(STDOUT_FILENO);
 	si->default_stderr = ft_dup(STDERR_FILENO);
-	si->isterminal = isatty(STDOUT_FILENO) 
+	si->isterminal = isatty(STDOUT_FILENO) \
 					&& isatty(STDIN_FILENO) \
 					&& isatty(STDERR_FILENO);
 	if (si->isterminal)
